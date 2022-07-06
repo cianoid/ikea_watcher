a0ba008a0471e89bdac1f4b98915462615e9c99e
Igor Shatava <cianoid@yandex.ru>
2022-07-06 21:07:55 +0300

    TG bot

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
+import asyncio
 import logging
+import os
 import re
 
 import requests
-from requests_cache import CachedSession
+from aiogram import Bot, types
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from requests_cache import CachedSession
 
 from configs import configure_logging
 from constants import MAIN_URL, USER_AGENT
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+GROUP_ID = os.environ.get('GROUP_ID')
 
 
 def get_response(url, use_cache=False):
@@ -36,10 +45,19 @@ def check_text_on_page(text):
     return False
 
 
-def main():
+async def main():
     configure_logging()
-    check_text_on_page('Прием новых заявок запустится немного позже.')
+
+    bot = Bot(token=TELEGRAM_TOKEN, parse_mode=types.ParseMode.HTML)
+
+    result = check_text_on_page('Прием новых заявок запустится немного позже.')
+
+    if not result:
+        await bot.send_message(
+            chat_id=GROUP_ID,
+            text=f'У ИКЕА, кажется, что-то поменялось. Возможно, открыта форма'
+                 f' - {MAIN_URL}')
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
